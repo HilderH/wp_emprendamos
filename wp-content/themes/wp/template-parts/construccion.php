05284931482fb8a1c1c71b2f8131b1d8584dbb1e
Hilder Hernandez <hilderclee@gmail.com>
2016-02-05 07:56:55 -0430

    imagenes

--- wp-content/themes/wp/template-parts/construccion.php
+++ wp-content/themes/wp/template-parts/construccion.php
@@ -16,10 +16,10 @@
 
 get_header(); ?>
 <div class="back1">
-	<div id="cont_1" style="background-image: url(<?php bloginfo( 'url' ); ?>/wp-content/themes/emprendamos/images/back1.png)">
+	<div id="cont_1" style="background-image: url(<?php bloginfo( 'url' ); ?>/wp-content/themes/wp/images/back1.png)">
 		<div class="text_area">
 			<div class="logo">
-				<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/emprendamos/images/logo.png">
+				<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/wp/images/logo.png">
 			</div>
 			<div class="slogan">
 				<h1>Emprendedores al servicio de emprendedores!</h1>
@@ -39,20 +39,20 @@ get_header(); ?>
 		<p>Ofrecemos financiamiento y capacitación para el emprendedor popular de la región.</p>
 		<ul>
 			<li>
-				<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/emprendamos/images/car.png">
+				<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/wp/images/car.png">
 				<span class="title">Comercial</span>
 			</li>
 			<li>
-				<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/emprendamos/images/car.png">
+				<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/wp/images/car.png">
 				<span class="title">Comercial</span>
 			</li>
 			<li>
-				<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/emprendamos/images/car.png">
+				<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/wp/images/car.png">
 				<span class="title">Comercial</span>
 			</li>
 
 		</ul>
-		<img class="line" src="<?php bloginfo( 'url' ); ?>/wp-content/themes/emprendamos/images/line.png">
+		<img class="line" src="<?php bloginfo( 'url' ); ?>/wp-content/themes/wp/images/line.png">
 		<h1 class="com_">como funciona</h1>
 		<h2 class="com">tres pasos muy sencillos.</h2>
 		<ul class="pasos">
@@ -80,11 +80,11 @@ get_header(); ?>
 <div class="back1">
 	<div id="cont3">
 		<div class="imgligt">
-			<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/emprendamos/images/ligth.png">
+			<img src="<?php bloginfo( 'url' ); ?>/wp-content/themes/wp/images/ligth.png">
 		</div>
 		<div class="textdown">Si estás buscando opciones para expandir las bases de tu negocio <span>¡Contáctanos!</span></div>
 		<a class="boton" href="">descarga tu planilla</a>
-		<img class="empr" src="<?php bloginfo( 'url' ); ?>/wp-content/themes/emprendamos/images/empr.png">
+		<img class="empr" src="<?php bloginfo( 'url' ); ?>/wp-content/themes/wp/images/empr.png">
 	</div>
 </div>
 
